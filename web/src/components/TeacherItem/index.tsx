a555f8598cbcd2e8bdf8e42e7ef70c555e60fcac
Myrian Katto <myriankatto@gmail.com>
2020-08-05 15:21:46 -0300

    thrid day

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,38 +1,47 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+interface teacherItemProps {
+  teacher: Teacher;
+}
+const TeacherItem: React.FC<teacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('/connections', { user_id: teacher.id });
+  }
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars1.githubusercontent.com/u/56876888?s=460&u=87fdfe4e603f612174de33a9cabd4234cca09969&v=4"
-          alt="Myrian Katto"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Myrian Katto</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. <br />
-        <br />
-        Et, iure quas? Porro nulla officiis non repellat doloribus pariatur, inventore vel.
-      </p>
+      <p>{teacher.bio}</p>
       <footer>
         <p>
-          Preço/hora <strong>R$ 80,00</strong>
+          Preço/hora <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a target="_blank"rel="noopener noreferrer" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
-}
+};
 
 export default TeacherItem;
